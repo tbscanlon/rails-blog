fb29238a37971ddfcc67d1c92c32803ca1cc3031
tbscanlon <thomas.scanlon@outlook.com>
2017-07-25 10:04:14 +0100

    Set default home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :articles, :welcome
+
+  get 'welcome/test'
+
   get 'welcome/index'
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
